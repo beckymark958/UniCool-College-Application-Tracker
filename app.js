b0a266a0e054354d2cac487087a98221a6d05dae
Becky Yu <beckymark958@gmail.com>
2022-06-21 15:04:51 -0400

    setup Mongodb

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
+// setup mongodb
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {       
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected!");
+});
+
+
+// setup views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+
 app.get('/', (req, res) => {
     res.render('home.ejs');
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: 'My Backgard', description: 'cheap camping!'});
+    await camp.save()
+    res.send(camp);
 })
 
 app.listen(3000, () => {
