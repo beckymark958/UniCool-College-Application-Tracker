85a62d98dc2d62ae9f3a3be3813700d70667c94e
Becky Yu <beckymark958@gmail.com>
2022-07-15 10:50:01 -0400

    Add custom map properties

--- models/campground.js
+++ models/campground.js
@@ -12,6 +12,8 @@ ImageSchema.virtual('thumbnail').get(function() {          // Use virtual proper
     return this.url.replace('/upload', '/upload/w_200');
 })
 
+const opts = { toJSON: {virtuals: true} };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -33,10 +35,16 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
-    reviews: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-        }]
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>`
 });
 
 // delete reviews when a campground is deleted
