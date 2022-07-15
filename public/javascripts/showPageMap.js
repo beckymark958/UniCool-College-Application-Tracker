0a9ad602dfaca507884b051b913b7dc5f0f5e660
Becky Yu <beckymark958@gmail.com>
2022-07-14 22:24:20 -0400

    Enable clicking on the map marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -12,4 +12,10 @@ map.on('style.load', () => {
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
     .addTo(map)
